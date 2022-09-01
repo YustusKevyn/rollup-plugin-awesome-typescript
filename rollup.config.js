b59a624655157043cda7fd412adb671cfa839a15
Yanis Morgenegg <yanis.morgenegg@icloud.com>
2022-09-01 15:35:31 +0200

    better declaration files

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,34 @@
 import { awesomeTypescript } from "rollup-plugin-awesome-typescript";
 import { version } from "./package.json";
 import replace from "@rollup/plugin-replace";
+import dts from "rollup-plugin-dts";
 
-export default {
-  input: "src/index.ts",
-  output: [
-    {
-      file: "lib/index.js",
-      format: "cjs"
-    },
-    {
-      file: "lib/index.mjs",
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: "lib/index.js",
+        format: "cjs"
+      },
+      {
+        file: "lib/index.mjs",
+        format: "esm"
+      }
+    ],
+    plugins: [
+      awesomeTypescript(),
+      replace({
+        VERSION: JSON.stringify(version)
+      })
+    ]
+  },
+  {
+    input: "src/index.ts",
+    output: {
+      file: "lib/index.d.ts",
       format: "esm"
-    }
-  ],
-  plugins: [
-    awesomeTypescript({
-      declarations: "lib/types"
-    }),
-    replace({
-      VERSION: JSON.stringify(version)
-    })
-  ]
-};
+    },
+    plugins: [dts()]
+  }
+];
